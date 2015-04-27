9c10d89be3043eeda38c460ceeecce33cdba8bc6
Nathaniel Rodrigues <nat_90311@yahoo.co.in>
2015-04-27 19:49:27 +0530

    Modified Git Ignore, Added a logging function, Started on Attribute mapping functionality

--- migration/migration.php
+++ migration/migration.php
@@ -8,14 +8,17 @@ class migration
     private $old_db;
     private $new_db;
     
-    function __construct()
+    function __construct($db=false)
     {
         //Get DB settings
         $ini = parse_ini_file("migration.ini",true);
         $this->old_db_settigs=$ini['old_database'];
         $this->new_db_settigs=$ini['new_database'];
-        $this->init_old();
-        $this->init_new();
+        if($db)
+		{
+			$this->init_old();
+			$this->init_new();
+		}
     }
     
     /*
@@ -48,8 +51,8 @@ class migration
         $db_name=$settings['database_name'];
         $username=$settings['user'];
         $password=$settings['password'];
-        print_r($settings);
-        $this->old_db = new PDO('mysql:host=$host;dbname=myDatabase', $username, $password);
+		$param=sprintf("mysql:host=%s;dbname=%s",$host,$db_name);
+        $this->old_db = new PDO($param, $username, $password);
         
     }
     /**
@@ -63,8 +66,8 @@ class migration
         $db_name=$settings['database_name'];
         $username=$settings['user'];
         $password=$settings['password'];
-        print_r($settings);
-        $this->new_db = new PDO('mysql:host=$host;dbname=myDatabase', $username, $password);
+		$param=sprintf("mysql:host=%s;dbname=%s",$host,$db_name);
+        $this->new_db = new PDO($param, $username, $password);
         
     }
     /*
@@ -86,9 +89,9 @@ class migration
 	*
 	*@return string
 	*/
-	getTableName($name)
+	public function getTableName($name)
 	{
-		return $name
+		return $name;
 	}
 	
 	
@@ -99,9 +102,51 @@ class migration
 	public function getAllCustomers()
 	{
 		$sql=sprintf("select email from %s GROUP BY email",$this->getTableName('customer_entity'));
-		$conn=$this->getOldDB;
+		$conn=$this->getOldDB();
 		$data=$conn->query($sql);
+		return $data->fetchAll(PDO::FETCH_COLUMN);
+	}
+	
+	/*
+	*@desc Gets customer attributes from the old database
+	*@return Associative array of Eav attribute table
+	*/
+	public function getExistingCustomerAttributes()
+	{
+		$sql=sprintf("SELECT * FROM %s WHERE entity_type_id=1",$this->getTableName('eav_attribute'));
+		$conn=$this->getOldDB();
+		$data=$conn->query($sql);
+		return $data->fetchAll(PDO::FETCH_ASSOC);
 	}
     
-    
+	/*
+	*@desc get corresponding attribute value in new database
+	*@param $attribute= associative array containing the folowing fields, attribute_code,backend_type,frontend_input, $
+	* attribute_type=1,2,3,4 (1= customer, 2=address,3=catalog, 4=product  values can be obtained from the eav_entity_type table)
+	*@return associative array containg Matched attribute_id,attribute_code,backend_type,frontend_input or 0 
+	*/
+	public function getMatchedAttribute($attribute,$attribute_type)
+	{
+		$sql=sprintf("SELECT * FROM %s WHERE entity_type_id=%d AND attribute_code=%s",$this->getTableName('eav_attribute'),$attribute_type,$attribute['attribute_code']);
+		$conn=$this->getNewDB();
+		$data=$conn->query($sql);
+		print_r($data);
+		$row=$data->fetchAll(PDO::FETCH_ASSOC);
+		return $row;
+		//$this->log('Test');
+	}
+	/*
+	*@desc Returns a list of attributes taht can be imported
+	*@return 
+	*/
+	public function getAttributesToImport()
+	{
+	}
+	private function log($message,$filename="status.log")
+	{
+		$handel=fopen($filename,'a');
+		fputs($handel,$message);
+		fclose($handel);
+	}
+	
 }
